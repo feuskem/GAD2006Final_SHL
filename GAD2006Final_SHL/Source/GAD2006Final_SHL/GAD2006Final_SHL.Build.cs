aec4e291592781fef8444b270ba5562a82278bdb
Ktyprk <Kutayperk@hotmail.com>
2024-01-06 23:31:13 +0300

    ai-continue

--- GAD2006Final_SHL/Source/GAD2006Final_SHL/GAD2006Final_SHL.Build.cs
+++ GAD2006Final_SHL/Source/GAD2006Final_SHL/GAD2006Final_SHL.Build.cs
@@ -8,7 +8,7 @@ public class GAD2006Final_SHL : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
